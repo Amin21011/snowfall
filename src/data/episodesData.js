2171e511e519085f1162cb6c0ae01395887d16a9
aminmousoun89@hotmail.com <aminmousoun89@hotmail.com>
2023-11-30 14:50:23 +0100

    season 1 toegevoegd

--- src/data/episodesData.js
+++ src/data/episodesData.js
@@ -1,29 +1,103 @@
-// episodesData.js
 const episodes = {
-    1: [
+
+    "seasons": [
       {
-        number: "1.1",
-        title: "Pilot",
-        DirectedBy: "Adil El Arbi & Bilall Fallah",
-        WrittenBy: "Dave Andron & Eric Amadio & John Singleton",
-        OriginalAirDate: "July 5, 2017",
-        Summary:
-          "In South Central Los Angeles, June of 1983, Franklin Saint sells marijuana for his uncle Jerome...",
-      },
-      // Add more episodes for season 1
-    ],
-    2: [
-      {
-        number: "2.1",
-        title: "Episode 1",
-        DirectedBy: "Director Name",
-        WrittenBy: "Writer Name",
-        OriginalAirDate: "Date",
-        Summary: "Summary for episode 2.1",
-      },
-      // Add more episodes for season 2
-    ],
-    // Add more seasons...
-  };
+        "number": 1,
+        "episodes": [
+          {
+            "overall": 1,
+            "season": 1,
+            "title": "Pilot",
+            "directed_by": "Adil El Arbi & Bilall Fallah",
+            "written_by": "Dave Andron & Eric Amadio & John Singleton",
+            "original_air_date": "July 5, 2017",
+            "summary": "In South Central Los Angeles, June of 1983, Franklin Saint sells marijuana for his uncle Jerome. His favorite wrestler, Gustavo 'El Oso' Zapata, does work on the side for the Mexican Villanueva Cartel, and is tasked with burglarizing a house by his contact, Pedro Nava..."
+          },
+          {
+            "overall": 2,
+            "season": 1,
+            "title": "Make Them Birds Fly",
+            "directed_by": "Adil El Arbi & Bilall Fallah",
+            "written_by": "Dave Andron & Eric Amadio & John Singleton",
+            "original_air_date": "July 12, 2017",
+            "summary": "Gustavo learns that he stole from Pedro's father Ramiro and the man he killed was his employee. Pedro and his cousin, Villanueva heiress Lucia, stole so they could buy cocaine from Alejandro and start their own business. Ramiro demands to his men that either the money be returned or the culprit be caught..."
+          },
+          {
+            "overall": 3,
+            "season": 1,
+            "title": "Slow Hand",
+            "directed_by": "Dan Attias",
+            "written_by": "Leonard Chang",
+            "original_air_date": "July 19, 2017",
+            "summary": "Franklin gets the muggers' names from Claudia and takes his best friend Leon Simmons to Compton, where they enlist local man Karvel to help find the thieves..."
+          },
+          {
+            "overall": 4,
+            "season": 1,
+            "title": "Trauma",
+            "directed_by": "Hiro Murai",
+            "written_by": "Emily Grinwis",
+            "original_air_date": "July 26, 2017",
+            "summary": "The man nearly kills Lucia when they attack him, but Gustavo narrowly saves her life and kills him. While Pedro convinces Ramiro that the man was the robber, Gustavo and Lucia bond..."
+          },
+          {
+            "overall": 5,
+            "season": 1,
+            "title": "seven-four",
+            "directed_by": "Lawrence Trilling",
+            "written_by": "Jerome Hairston",
+            "original_air_date": "August 2, 2017",
+            "summary": "On the Fourth of July, Teddy and Alejandro's plane breaks down in a Mexican desert and Alejandro is rendered unconscious from a head injury. He eventually wakes up and flies them back to the U.S., while Teddy gives him the photo..."
+          },
+          {
+            "overall": 6,
+            "season": 1,
+            "title": "A Long Time Coming",
+            "directed_by": "Michael Lehmann",
+            "written_by": "Tatiana Suarez-Pico",
+            "original_air_date": "August 9, 2017",
+            "summary": "Teddy goes to D.C. regarding a shipment of Contra guns and has sex with Julia despite their recent estrangement. She warns him not to hide any more of the truth from her. Gustavo takes Lucia to his estranged brother Hernan and asks to meet with Eduardo 'Stomper' Castillo, the boss of the Los Monarcas gang..."
+          },
+          {
+            "overall": 7,
+            "season": 1,
+            "title": "Cracking",
+            "directed_by": "Zetna Fuentes",
+            "written_by": "Aaron Slavick",
+            "original_air_date": "August 16, 2017",
+            "summary": "Pedro observes Gustavo and Lucia having sex and later asks to talk to Ramiro. Alejandro and Teddy trace the number on Kristen's poster to her apartment where they spot her sister Victoria, who put up the poster..."
+          },
+          {
+            "overall": 8,
+            "season": 1,
+            "title": "Baby Teeth",
+            "directed_by": "Solvan 'Slick' Naim",
+            "written_by": "Story by: Margaret Rose Lester, Teleplay by: Emmy Grinwis & Sal Calleros",
+            "original_air_date": "August 23, 2017",
+            "summary": "Franklin convinces Kevin and Jerome to distribute crack for free and get people interested, but has a fight with Leon over payment. He and Kevin find that they have a horde of customers waiting to buy the next day..."
+          },
+          {
+            "overall": 9,
+            "season": 1,
+            "title": "Story of a Scar",
+            "directed_by": "Meera Menon",
+            "written_by": "Leonard Chang & Jerome Hairston",
+            "original_air_date": "August 30, 2017",
+            "summary": "Franklin returns home from the hospital to find his mother Sharon 'Cissy' Saint waiting for him. She accuses him of selling marijuana for Jerome and kicks him out. He gets the crack from Melody's closet, leaving her outraged that he put her at risk..."
+          },
+          {
+            "overall": 10,
+            "season": 1,
+            "title": "The Rubicon",
+            "directed_by": "John Singleton",
+            "written_by": "Dave Andron",
+            "original_air_date": "September 6, 2017",
+            "summary": "Franklin lets Jerome join him. He returns home to get his things, learning that Cissy has been fired from her job and leaving drug money for her. Avi raises the price on cocaine when Jerome mouths off to him..."
+          }
+        ]
+      }
+      // Voeg hier de rest van de seizoenen toe
+    ]
+  }
 
   export default episodes;
